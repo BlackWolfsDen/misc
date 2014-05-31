e53d40b945c2db550c9b69f4dd5fda37fb18acc4
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-05-30 20:54:29 -0600

    Update Guild_Leveling_System.lua

--- Guild_Leveling_System.lua
+++ Guild_Leveling_System.lua
@@ -1,8 +1,7 @@
 -- Guild Level new/missed guild Work-around . requires Eluna since TC cant get its head outta its ass
 -- And Eluna events seem to fire consistantly
 -- hey you wanted a fix so either wait, or use Eluna.  --?GOT ELUNA?--
--- just add to your scripts folder and restart . any newly created guild created will get added
--- to the sql table.
+-- just add to your scripts folder and restart . any newly created guild created will get added to the sql table.
 -- or a if a player logs in belonging to a guild not yet added. then this will add it.
 GuildLevel = {};
 
