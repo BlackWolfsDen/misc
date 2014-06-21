042905a00401a21678773ca2f85659ed2daf0582
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-21 09:00:53 -0600

    Update NPC_Emote.lua

--- NPC_Emote.lua
+++ NPC_Emote.lua
@@ -1,5 +1,5 @@
--- this is supposed to hook the script to creature event 8 on recieve emote
--- if the emote matches a pre-determined emote value then a pre-determined return emote
+-- this is supposed to hook the script to creature event 8 on receive emote
+-- if the emote matches a random pre-determined emote value then a random pre-determined return emote
 -- will happen. ooorrrrr... the creature may just attack you.... its a work in progress
 -- slp13at420 emudevs.com
 
