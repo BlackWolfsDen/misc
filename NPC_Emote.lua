f0dade3377b51884f5734b500c6701c6b0a47c72
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-21 13:04:07 -0600

    Update NPC_Emote.lua

--- NPC_Emote.lua
+++ NPC_Emote.lua
@@ -3,23 +3,20 @@
 -- will happen. ooorrrrr... the creature may just attack you.... its a work in progress
 -- slp13at420 emudevs.com
 
-local Emotemax = 20
+local Emotemax = 470
 local percent = 0.25
 local NPCEMOTEIDS = {}
 local NPCEMOTEIDS = {{3100},{3116},{3127},{10685}};
 				
 local function NPC_EMOTE(event, creature, player, emoteid)
 
-local Reactor = math.random(1, Emotemax)
-local Reaction = math.random(1, Emotemax+1)
+local Reaction = math.random(1, Emotemax)
 
-	if(emoteid==Reactor)then
-		if(Reaction < (Emotemax-(Emotemax*percent)))then
-			creature:Emote(Reaction)
-		else
-			creature:SendUnitYell("!!How Dare You!!",0)
-			creature:AttackStart(player)
-		end
+	if(Reaction < (Emotemax-(Emotemax*percent)))then
+		creature:Emote(Reaction)
+	else
+		creature:SendUnitYell("!!How Dare You!!",0)
+		creature:AttackStart(player)
 	end
 end
 
