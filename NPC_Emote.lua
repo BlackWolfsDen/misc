d517a45715f9d8b28f3643069f9ab953ff6e77a0
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-21 11:59:24 -0600

    Update NPC_Emote.lua
    added SendUnitYell(msg,lang) used 1 for lang value but dunno if its for universal altho orc warrior can read it...

--- NPC_Emote.lua
+++ NPC_Emote.lua
@@ -17,6 +17,7 @@ local Reaction = math.random(1, Emotemax+1)
 		if(Reaction < (Emotemax-(Emotemax*percent)))then
 			creature:Emote(Reaction)
 		else
+			creature:SendUnitYell("!!How Dare You!!",1)
 			creature:AttackStart(player)
 		end
 	end
