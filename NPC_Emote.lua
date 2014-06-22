d0f979e643567050c7c93b59e759434fe09f634c
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-06-21 19:03:20 -0600

    Update NPC_Emote.lua

--- NPC_Emote.lua
+++ NPC_Emote.lua
@@ -1,13 +1,13 @@
 -- tnx to lightning blade for the use of method example that kicked off the idea :D.
 -- this is supposed to hook the script to creature event 8 on receive emote
--- if the emote matches a random pre-determined emote value then a random pre-determined return emote
--- will happen. ooorrrrr... the creature may just attack you.... its a work in progress
+-- when a pre-determined creature recieves an emote then it will return a random pre-determined emote
+-- ooorrrrr... the creature may just attack you.... its a work in progress
 -- slp13at420 emudevs.com
 
 local Emotemax = 470
 local percent = 0.25
 local NPCEMOTEIDS = {}
-local NPCEMOTEIDS = {{3100},{3116},{3127},{10685}};
+local NPCEMOTEIDS = {{3100},{3116},{3127},{10685}};-- creature entry id's
 				
 local function NPC_EMOTE(event, creature, player, emoteid)
 
