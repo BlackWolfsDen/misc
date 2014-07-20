c20b96a18495141fc47c858c910c728776633a09
Grumbo <blackwolfsden@hotmail.com>
2014-07-19 21:30:04 -0600

    Update CLass_Item_Giver_First_Login.lua
    this will add tabled gear by item id's to new characters based on class.

--- vendors/CLass_Item_Giver_First_Login.lua
+++ vendors/CLass_Item_Giver_First_Login.lua
@@ -1,9 +1,11 @@
 --[[
+For Trinity Core2 Eluna
 by slp13at420 of EmuDevs
+Gives new characters (class based) gear on first time login.
+just edit the entries with real item id's. add more ore take some away .. its dynamic .. it WILL adapt.
 ]]--
-
 local StartGear = {
-		[1] = {123, 456, 789, 147, 258, 369}, -- Warrior
+		[1] = {20558, 456, 789, 147, 258, 369}, -- Warrior
 		[2] = {123, 456, 789, 147, 258, 369}, -- Paladin
 		[3] = {123, 456, 789, 147, 258, 369}, -- Hunter
 		[4] = {123, 456, 789, 147, 258, 369}, -- Rogue
@@ -17,11 +19,12 @@ local StartGear = {
 					};
 
 function Class_Gear(event, player)
-	for _, v in ipairs(StartGear) do
-		if(player:GetItemCount(v) < 1)then
+	for _, v in ipairs(StartGear[player:GetClass()]) do
+print(v)
+		if((player:GetItemCount(v)) == (0 or nil))then
 			player:AddItem(v, 1)
 		end
 	end
 end
 		
-RegisterPlayerEvent(30, Class_Gear) -- event 30 == on first login. event 3 == on login
+RegisterPlayerEvent(30, Class_Gear) -- event 30 == on first login.
