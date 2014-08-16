fd0a20e51c582d178e0308fad624ccc3e422207a
Grumbo <blackwolfsden@hotmail.com>
2014-08-16 08:34:46 -0700

    Create new file

---
+++ teleporters/rochet/SQL_Teleporter/pocket_Gnome_Despawner.lua
@@ -0,0 +1,15 @@
+-- by slp13at420 of EmuDevs.com for Black Wolfs Den's pocket gnome
+
+local npcid = 128
+local timer = 90000
+
+local function DespawnPocketGnome(event, delay, cycle, unit)
+unit:RemoveEvents()
+unit:DespawnOrUnsummon()
+end
+
+local function DespawnTimer(event, unit)
+unit:RegisterEvent(DespawnPocketGnome, timer, 1)
+end
+
+RegisterCreatureEvent(npcid, 23, DespawnTimer)
