d8cc698851dc42bc0701846c5ceb8c167ffcb9ea
Grumbo <blackwolfsden@hotmail.com>
2014-08-24 18:55:26 -0700

    Update GM_Creature_Respawner_with_cooldown_timer.lua

--- GM_Creature_Respawner_with_cooldown_timer.lua
+++ GM_Creature_Respawner_with_cooldown_timer.lua
@@ -1,5 +1,5 @@
--- by slp13at420 of EmuDevs.com 
--- idea by 125125 of EmuDevs.com
+-- request by 125125 of EmuDevs.com
+-- created by slp13at420 of EmuDevs.com 
 
 local Crespawntimer = {};
 local message = "#respawn" -- trigger message
