d63036c6c30df4b443f425ebc7eb6f806901585f
Grumbo <blackwolfsden@hotmail.com>
2014-09-25 19:53:38 -0700

    Create new file

---
+++ Eluna_Load_Start_Stop/zzzz ELUNA LOAD FINISH MARKER.lua
@@ -0,0 +1,7 @@
+ElunaEngineLoad[2] = os.clock()
+local duration = (string.format("Time taken: %0.02f seconds.", ElunaEngineLoad[2] - ElunaEngineLoad[1]));
+print("\n")
+print("**********************")
+print("ELUNA SCRIPTS: Loaded.")
+print(duration)
+print("**********************")
