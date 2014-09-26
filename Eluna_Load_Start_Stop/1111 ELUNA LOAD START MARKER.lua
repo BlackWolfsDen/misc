9493a88a2be2215402c2508b5f0ba4cc00d03bb5
Grumbo <blackwolfsden@hotmail.com>
2014-09-25 19:52:17 -0700

    Create new file

---
+++ Eluna_Load_Start_Stop/1111 ELUNA LOAD START MARKER.lua
@@ -0,0 +1,6 @@
+print("***********************")
+print("ELUNA SCRIPTS: Loading.")
+print("Engine: "..GetLuaEngine())
+print("***********************")
+ElunaEngineLoad = {};
+ElunaEngineLoad[1] = os.clock()
