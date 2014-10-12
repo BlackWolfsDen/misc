d6a2f1e34b925c6c4bced6deeb2b30426194dba5
Grumbo <blackwolfsden@hotmail.com>
2014-10-11 23:13:47 -0700

    Create sequential questioning.sql

---
+++ sql/gossip-menu/sequential questioning.sql
@@ -0,0 +1,21 @@
+-- Gossip Menu
+DELETE FROM `gossip_menu` WHERE `entry` IN (11461, 11462, 11463);
+INSERT INTO `gossip_menu` (`entry`, `text_id`) VALUES
+(11461, 0),
+(11462, 15987),
+(11463, 15988),
+(11464, 15989);
+
+-- Gossip Menu Option
+DELETE FROM `gossip_menu_option` WHERE `menu_id` IN (11461, 11462, 11463);
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `box_coded`, `box_money`, `box_text`) VALUES
+(11461, 0, 0, "Why are you stealing eggs?", 1, 1, 11462, 0, 0, 0, ''),
+(11462, 0, 0, "Who? Who are you giving the eggs to?", 1, 1, 11463, 0, 0, 0, ''),
+(11463, 0, 0, "Brood of the Earth-Warder... you answer to a black dragon? Give me a name!", 1, 1, 11464, 0, 0, 0, '');
+
+-- NPC Text
+DELETE FROM `npc_text` WHERE `ID` IN (15987, 15988, 15989);
+INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `lang1`, `prob1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `lang2`, `prob2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `lang3`, `prob3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `lang4`, `prob4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `lang5`, `prob5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `lang6`, `prob6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `lang7`, `prob7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`, `WDBVerified`) VALUES
+(15987, '', "She asked for eggs. Eggs for her fiery hatchery in another realm.$B$BAnd one egg... a special egg... she sought more than any other.", 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, 15211),
+(15988, '', "She of the skies, but also of darkness. Brood of the Earth-Warder. Invincible!",  0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, 15211),
+(15989, '', "Foolish $r! There is a new order. Aviana is dead. DEAD! And never coming back.$B$BTell that to the druids in the trees.$B$BSethria will soon be the master of the skies, and she has made sure Aviana will never be reborn. The egg is hers.$B$BYou are too late.", 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, 15211);
