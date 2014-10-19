db1b0dc604fe905309ca0eef5f385b174b019084
Grumbo <blackwolfsden@hotmail.com>
2014-10-19 10:46:18 -0700

    Update Yelling_npc_Script_Auto.lua

--- Yelling_npc_Script_Auto.lua
+++ Yelling_npc_Script_Auto.lua
@@ -14,25 +14,26 @@ local  ANN = {};
 -- stated // say = 0 // yell = 1 
 -- linked // table key id if your using multiple statements  for one announcement i.e.(yell THEN say)
 -- emote // talk = 1 // yell = 5// Question = 6 // Dance = 10 // Rude = 14 // shout = 22 // 
+-- spell // spell id
 -- http://collab.kpsn.org/display/tc/Emote
 
 ANN[npcid] = {
-	[1] = {"Well,, that was dumb.", 0, 0, 1},
-	[2] = {"!Bite my shiney metal ass!", 1, 0, 14},
+	[1] = {"Well,, that was dumb.", 0, 0, 1, 0},
+	[2] = {"!Bite my shiney metal ass!", 1, 0, 14, 0},
 	[3] = {"Well,, were boned.", 0, 0, 1},
-	[4] = {"Hey sexy momma .. Wanna kill all humans..??.", 0, 0, 1},
-	[5] = {"Goodbye losers whom I allways hated", 1, 0, 22},
-	[6] = {"!Shut the hell up!", 1, 0, 5},
+	[4] = {"Hey sexy momma .. Wanna kill all humans..??.", 0, 0, 1, 0},
+	[5] = {"Goodbye losers whom I allways hated", 1, 0, 22, 0},
+	[6] = {"!Shut the hell up!", 1, 0, 5, 0},
 	[7] = {"Would you kindly shut your noise hole?", 0, 0, 1},
-	[8] = {"I'm gonna go build my own theme park.. with blackjack and hookerz.", 0, 101, 1},
-	[9] = {"Who are you and why should i care?", 0, 0, 6},
-	[10] = {"!Shut up and Pay attention To Me !!.. !!BENDER!!", 1, 0, 5},
-	[11] = {"Hasta La Vista , Meat bag.", 0, 0, 1},
-	[12] = {"Awww, heres a little song i wrote to cheer you up. Its called ", 0, 100, 1},
-	[13] = {"Do the Bender ,, Do the Bender ,, its your birthday ,, do the bender", 0, 0, 10},
-	[14] = {"Shut up baby , you love it", 0, 0, 1},
-	[100] = {"!!Let's go allready!!", 1, 0, 5}, -- linked
-	[101] = {"In fact ,, forget the park.", 0, 0, 1}, -- linked
+	[8] = {"I'm gonna go build my own theme park.. with blackjack and hookerz.", 0, 101, 1, 0},
+	[9] = {"Who are you and why should i care?", 0, 0, 6, 0},
+	[10] = {"!Shut up and Pay attention To Me !!.. !!BENDER!!", 1, 0, 5, 0},
+	[11] = {"Hasta La Vista , Meat bag.", 0, 0, 1, 0},
+	[12] = {"Awww, heres a little song i wrote to cheer you up. Its called ", 0, 100, 1, 0},
+	[13] = {"Do the Bender ,, Do the Bender ,, its your birthday ,, do the bender", 0, 0, 10, 0},
+	[14] = {"Shut up baby , you love it", 0, 0, 1, 0},
+	[100] = {"!!Let's go allready!!", 1, 0, 5, 0}, -- linked
+	[101] = {"In fact ,, forget the park.", 0, 0, 1, 0}, -- linked
 		};
 		
 local function Drop_Event_On_Death(eventid, creature, killer)
@@ -45,14 +46,21 @@ local function Announce(id, creature)
 	if(ANN[npcid][id][4] ~= (nil or 0))then
 		creature:Emote(ANN[npcid][id][4])
 	end
+
 	if(ANN[npcid][id][2] == 0)then
 		creature:SendUnitSay(ANN[npcid][id][1], 0)
 	end
+
 	if(ANN[npcid][id][2] == 1)then
 		creature:SendUnitYell(ANN[npcid][id][1], 0)
 	end
+
 	if(ANN[npcid][id][3]~=(nil or 0))then
 		Announce(ANN[npcid][id][3], creature)
+	end
+
+	if(ANN[npcid][id][5] ~= (nil or 0))then
+		creature:CastSpell(ANN[npcid][id][5])
 	end
 end
 
