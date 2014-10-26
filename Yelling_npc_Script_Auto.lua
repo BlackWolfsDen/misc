69e68ffaff75f4c4f8e9894b436dfb9a1eb2cdc1
Grumbo <blackwolfsden@hotmail.com>
2014-10-25 21:11:28 -0700

    Update Yelling_npc_Script_Auto.lua

--- Yelling_npc_Script_Auto.lua
+++ Yelling_npc_Script_Auto.lua
@@ -76,7 +76,7 @@ ANN[cGuid] = {reset = 2, link = 0};
 		if(spawn_type == 1)then 
 			PerformIngameSpawn(spawn_type, spawn_id, creature:GetMapId(), 0, creature:GetX()+2, creature:GetY(), creature:GetZ(), creature:GetO(), 0, spawn_duration, -1) -- perfect
 		else
-			PerformIngameSpawn(spawn_type, spawn_id, creature:GetMapId(), 1, creature:GetX()+2, creature:GetY(), creature:GetZ(), creature:GetO(), 1, spawn_duration, -1)
+			PerformIngameSpawn(spawn_type, spawn_id, creature:GetMapId(), 0, creature:GetX()+2, creature:GetY(), creature:GetZ(), creature:GetO(), 1, spawn_duration, -1)
 		end
 	else
 	end
@@ -97,7 +97,15 @@ local spawn_type, spawn_id = table.unpack(ANN["Bender"][id][6])
 
 	if(spellid ~= (nil or 0))then creature:CastSpell(creature, spellid); end-- check the spellid column for spell id.
 
-	if(spawn_type ~= (nil or 0))then PerformIngameSpawn(spawn_type, spawn_id, creature:GetMapId(), 0, creature:GetX()+2, creature:GetY(), creature:GetZ(), creature:GetO(), 0, spawn_duration, -1); end
+	if(spawn_type ~= (nil or 0))then 
+
+		if(spawn_type == 1)then 
+			PerformIngameSpawn(spawn_type, spawn_id, creature:GetMapId(), 0, creature:GetX()+2, creature:GetY(), creature:GetZ(), creature:GetO(), 0, spawn_duration, -1) -- works perfect
+		else
+			PerformIngameSpawn(spawn_type, spawn_id, creature:GetMapId(), 0, creature:GetX()+2, creature:GetY(), creature:GetZ(), creature:GetO(), 1, spawn_duration, -1) -- spawns but no despawn
+		end
+	else
+	end
 
 	ANN[cGuid] = {reset = 2,};
 
