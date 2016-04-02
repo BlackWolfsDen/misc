98551d64552d34c02fc78152f4f2cbd1168b64bb
Grumbo <blackwolfsden@hotmail.com>
2016-04-01 19:35:41 -0600

    Create CPP-Creature_Kill_Rewarder.cpp

---
+++ CPP-Creature_Kill_Rewarder.cpp
@@ -0,0 +1,33 @@
+// By slp13at420 of EmuDevs.com
+
+std::string DB_COLUMN = "reward_count"; // this is the name of the element in auth.account you added to store this value.
+
+void StoreCredit(uint32 id, uint32 count);
+{
+	LoginDatabase.PExecute("UPDATE account SET `%s` = '%u' WHERE `id` = '%u';", DB_COLUMN.c_str(), count, id);
+};
+
+class KillCreatureCredit : public CreatureScript
+{
+public: KillCreatureCredit() : CreatureScript("KillCreatureCredit"){ }
+
+	struct GGW_GUILD_CANNON_AI : public ScriptedAI
+	{
+		void JustDied(Unit* unit)
+		{
+			if (unit->ToPlayer())
+			{
+				StoreCredit(player->GetSession()->GetAccountId(), 2);
+			}
+		}
+	}
+	CreatureAI* GetAI(Creature* creature)const override
+	{
+		return new KillCreatureCredit(creature);
+	}
+};
+
+void AddSC_KillCreatureRewarder()
+{
+	new KillCreatureCredit;
+}
